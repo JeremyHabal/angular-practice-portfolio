be7e336502a4f8aaaed4e69bb36147589b57865a
Jeremy Ryan Habal <software.devintern8@dice205.com>
2022-12-20 09:42:58 +0800

    contact us progress update

--- src/app/components/works-projects/works-projects.component.ts
+++ src/app/components/works-projects/works-projects.component.ts
@@ -3,8 +3,41 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-works-projects',
   templateUrl: './works-projects.component.html',
-  styleUrls: ['./works-projects.component.scss']
+  styleUrls: ['./works-projects.component.scss'],
 })
 export class WorksProjectsComponent {
+  worksProjects: any;
+  worksProjectsArray: any;
 
+  constructor() {
+    this.worksProjects = [
+      {
+        name: 'Seoul-Soul',
+        category: 'Web Development & UI Design',
+        description: 'An e-commerce website',
+        tags: ['Java', 'Bootstrap', 'Adobe XD'],
+        image: './assets/img/works/Seoul-Soul.jpg',
+        link: '',
+        github: 'https://github.com/jeremyhabal/angular-practice-portfolio.git',
+      },
+      {
+        name: 'WarpZone',
+        category: 'Web Development & UI Design',
+        description: 'An e-commerce website',
+        tags: ['MySQL', 'Bootstrap'],
+        image: './assets/img/works/WarpZone.jpg',
+        link: '',
+        github: '',
+      },
+      {
+        name: 'Surveyor',
+        category: 'App Development & UI Design',
+        description: 'A capstone project',
+        tags: ['React', 'Tailwind'],
+        image: './assets/img/works/Surveyor.jpg',
+        link: '',
+        github: '',
+      },
+    ];
+  }
 }
